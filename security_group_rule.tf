617fa332043ea0c905248c3d010b3a0bd28dce51
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-01-20 00:09:43 +0900

    セキュリティグループにコメントを追加

--- security_group_rule.tf
+++ security_group_rule.tf
@@ -1,6 +1,8 @@
+# セキュリティグループ(=AWSにおけるファイアウォールの役割)
+## インバウンド通信(サーバーに入ってくる通信)
 resource "aws_security_group_rule" "ec2-sg-rule1" {
-  type              = "ingress" #インバウンド通信に関するルール
-  from_port         = 22
+  type              = "ingress" #インバウンド通信に関するルール(通して良い通信を指定する)
+  from_port         = 22 #sshが使用するポート番号は22番と決まっている。(22番だけ通していいと宣言)
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -8,7 +10,7 @@ resource "aws_security_group_rule" "ec2-sg-rule1" {
 }
 
 resource "aws_security_group_rule" "ec2-sg-rule2" {
-  type              = "ingress"
+  type              = "ingress" #インバウンド通信に関するルール
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
@@ -16,6 +18,8 @@ resource "aws_security_group_rule" "ec2-sg-rule2" {
   security_group_id = aws_security_group.ec2-sg.id
 }
 
+## アウトバウンド(サーバーから出ていく通信)
+### インバウンドはホワイトリストで管理するのに対して、アウトバウンドは全部許可してしまうことが多い
 resource "aws_security_group_rule" "ec2-sg-rule3" {
   type              = "egress" #アウトバウンド通信(外部のコンピュータへのアクセス)を許可
   from_port         = 0
@@ -25,8 +29,9 @@ resource "aws_security_group_rule" "ec2-sg-rule3" {
   security_group_id = aws_security_group.ec2-sg.id
 }
 
+# rds用のセキュリティグループ
 resource "aws_security_group_rule" "rds-sg-rule" {
-  type                     = "ingress"
+  type                     = "ingress" #インバウンド通信に関するルール
   from_port                = 3306
   to_port                  = 3306
   protocol                 = "tcp"
