0b5214a3028b676e2ea521d96a05402f2745c629
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-02-15 15:52:45 +0000

    aws_cognito_user_pool_client

--- cognito.tf
+++ cognito.tf
@@ -36,3 +36,99 @@ resource "aws_cognito_user_pool" "user_pool" {
         case_sensitive = false
     }
 }
+
+# Google OAuth
+variable "google_client_id" {}
+variable "google_client_secret" {}
+
+resource "aws_cognito_identity_provider" "example_provider" {
+    user_pool_id  = "${aws_cognito_user_pool.user_pool.id}"
+    provider_name = "Google"
+    provider_type = "Google"
+
+    provider_details = {
+        authorize_scopes  = "email"
+        client_id         = var.google_client_id
+        client_secret     = var.google_client_secret
+    }
+
+    attribute_mapping = {
+        email    = "email"
+        "username" = "sub"
+    }
+}
+
+resource "aws_cognito_user_pool_client" "user_pool_client" {
+        user_pool_id  = "${aws_cognito_user_pool.user_pool.id}"
+        name = "SomeWebApp"
+
+  access_token_validity                = 60
+  allowed_oauth_flows                  = ["code","implicit"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes                 = ["openid"]
+  callback_urls                        = ["http://localhost:3000/"]
+
+  explicit_auth_flows = [
+    "ALLOW_CUSTOM_AUTH",
+    # 更新トークン(新しいアクセストークンを取得するのに必要。)
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    # SRPプロトコルを使用してユーザー名&パスワードを検証する。
+    "ALLOW_USER_SRP_AUTH",
+  ]
+  id_token_validity                    = 60
+  logout_urls                   = ["http://localhost:3000/"]
+  prevent_user_existence_errors = "ENABLED"
+
+  # 属性の読み取り有無設定。
+  read_attributes = [
+    "address",
+    "birthdate",
+    "email",
+    "email_verified",
+    "family_name",
+    "gender",
+    "given_name",
+    "locale",
+    "middle_name",
+    "name",
+    "nickname",
+    "phone_number",
+    "phone_number_verified",
+    "picture",
+    "preferred_username",
+    "profile",
+    "updated_at",
+    "website",
+    "zoneinfo",
+  ]
+  # 更新トークンの期限
+  refresh_token_validity       = 30
+  supported_identity_providers = ["COGNITO","Google"]
+
+  # 属性の書き有無設定。
+  write_attributes = [
+    "address",
+    "birthdate",
+    "email",
+    "family_name",
+    "gender",
+    "given_name",
+    "locale",
+    "middle_name",
+    "name",
+    "nickname",
+    "phone_number",
+    "picture",
+    "preferred_username",
+    "profile",
+    "updated_at",
+    "website",
+    "zoneinfo",
+  ]
+
+    token_validity_units {
+        access_token  = "minutes"
+        id_token      = "minutes"
+        refresh_token = "days" 
+    }
+}
